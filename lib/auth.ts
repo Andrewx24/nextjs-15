b3f053ca0cbdb4010c8d03cc6da46a6bbd94d97c
Andrew Aliaj <andyaliaj@gmail.com>
2024-11-19 10:38:10 -0500

    updates

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,34 @@
-import { User } from "./types";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-
+export const authConfig: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, token, user }) {
+      if (token?.sub) {
+        session.user.id = token.sub as string;
+      }
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+  },
+};
